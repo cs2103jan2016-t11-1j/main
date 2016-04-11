378827f4c3902318b2d5087f786ec485aca50b12
Utkarsh Jain <mail.utkarshjain@gmail.com>
2016-04-11 21:49:42 +0800

    Some tests readded

---
+++ src/tests/Tests.java
@@ -0,0 +1,132 @@
+/**
+ * @@author A0130157R
+ */
+package tests;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import com.joestelmach.natty.generated.DateParser;
+
+import controller.CommandInterpreter;
+import javafx.scene.control.ScrollPane;
+import model.TodoFile;
+
+public class Tests {
+	protected static CommandInterpreter cmdInt = null;
+	protected static TodoFile todos = null;
+
+	private void execString(String s) {
+		//cmdInt.setLastCommand();
+		cmdInt.executeCommand();
+	}
+
+	@Before
+	public void setUp() {
+		todos = new TodoFile("test.txt");
+		ScrollPane scrollpane = null;
+		cmdInt = new CommandInterpreter(todos, scrollpane);
+	}
+
+	@After
+	public void tearDown() {
+		todos.clear();
+		todos.exit();
+	}
+
+	@Test
+	public void testAdd1() {
+		execString("add go to party #1 tomorrow night at 10");
+		execString("display");
+	}
+
+	@Test 
+	public void testAdd() { execString("add arst"); 
+	execString("add tsra"); 
+	execString("add neio"); 
+	execString("display");
+	//assertEquals(cmdInt.getEntry(0).getContents(), "arst");
+	//assertEquals(cmdInt.getEntry(1).getContents(), "tsra");
+	//assertEquals(cmdInt.getEntry(2).getContents(), "neio"); }
+	}
+
+	@Test 
+	public void testDelete() { 
+		execString("add arst"); 
+		execString("add tsra"); 
+		execString("add neio"); 
+		execString("display");
+		//assertEquals(cmdInt.getEntry(0).getContents(), "arst"); 
+		execString("delete 1"); 
+		//assertEquals(cmdInt.getEntry(0).getContents(), "tsra");
+		//assertEquals(cmdInt.getEntry(1).getContents(), "neio"); 
+		execString("delete 2"); 
+		//assertEquals(cmdInt.getEntry(0).getContents(), "tsra");
+		execString("delete 1"); 
+		assertEquals(todos.isEmpty(), true);
+		execString("display"); 
+	}
+
+	@Test 
+	public void testClear() { 
+		execString("add arst"); 
+		execString("add tsra"); 
+		execString("add neio");
+		//assertEquals(cmdInt.getEntry(0).getContents(), "arst");
+		//assertEquals(cmdInt.getEntry(1).getContents(), "tsra");
+		//assertEquals(cmdInt.getEntry(2).getContents(), "neio");			
+		execString("clear"); 
+		assertEquals(todos.isEmpty(), true); 
+	}
+
+	@Test
+	public void testSort() { 
+		execString("add z"); 
+		execString("add a");
+		execString("add z"); 
+		execString("add y"); 
+		execString("add b");
+		execString("sort"); 
+		//assertEquals(cmdInt.getEntry(0).getContents(), "a");
+		//assertEquals(cmdInt.getEntry(1).getContents(), "b");
+		//assertEquals(cmdInt.getEntry(2).getContents(), "y");
+		//assertEquals(cmdInt.getEntry(3).getContents(), "z");
+		//assertEquals(cmdInt.getEntry(4).getContents(), "z"); 
+	}
+
+	@
+	Test public void testDateParser() { 
+		DateParser natty = new DateParser(null);
+		String tmr = "tomorrow";
+		try{ System.out.println("Date = " + natty.parse());
+		System.out.println("Date = " + natty.parse()); }
+		catch (Exception e){ 
+			//e.printStackTrace(); 
+		} }
+
+	@Test
+	public void testSearch() { 
+		execString("add z"); execString("add a");
+		execString("add z"); 
+		execString("add y"); 
+		execString("add b");
+		execString("search a"); 
+		execString("search z"); 
+		execString("search b");
+		execString("search y"); 
+	}
+
+	/*
+      @Test public void testGetStuff() { cmdInt.executeCommand("add go to party #1 tomorrow night at 10");
+      //assertEquals(cmdInt.getStuff(), "1");
+      System.out.println(cmdInt.getStuff()); }
+	 */
+}
